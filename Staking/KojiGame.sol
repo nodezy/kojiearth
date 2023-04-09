59b89530a2c93761983b5bd9b9a94f8a5c5ba55a
Nodezy <80292753+nodezy@users.noreply.github.com>
2023-04-08 23:27:10 -0400

    Update KojiGame.sol

--- Staking/KojiGame.sol
+++ Staking/KojiGame.sol
@@ -3,14 +3,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/introspection/ERC165.sol"; 
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 interface IAuth {
@@ -22,10 +19,14 @@ interface IAuth {
 
 // Interface for the Koji Oracle
 interface IOracle {
- function getMintUSD(uint256 amount) external view returns (uint256);
+    function getMintUSD(uint256 amount) external view returns (uint256);
 }
 
-contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
+interface IGamePass {
+    function getNFTAuth(address _holder) external view returns (bool);
+}
+
+contract KojiGame is ERC721, Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
     using SafeMath for uint256;
 
@@ -38,25 +39,42 @@ contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
     Counters.Counter public _NFTIds;
 
     mapping(uint256 => mapping(address => bool)) public nftMinted; //nftMinted[_nftID][recipient]
-    mapping(address => bool) public nftAuth; //nftAuth[recipient]
-
+    
     string URI;
 
     IAuth private auth;
     IOracle private oracle;
+    IGamePass private gamepass;
 
     address  AUTH;
     address  DEAD;
     address  ORACLE; 
-
+    address GAMEPASS;
+    
     struct Achievements {
         string name; // Name of the achievement
         string uri; // Address of NFT metadata
     }
+    
+    // Mapping from owner to list of owned token IDs
+    mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
 
-    mapping(uint256 => Achievements) private achievements; // Info of each NFT
+    // Mapping from token ID to index of the owner tokens list
+    mapping(uint256 => uint256) private _ownedTokensIndex;
 
-    constructor(address _auth, string memory _uri, string[] memory strings) ERC721("SPACEWARS", "v1.Achievement") { 
+    // Array with all token ids, used for enumeration
+    uint256[] private _allTokens;
+
+    // Mapping from token id to position in the allTokens array
+    mapping(uint256 => uint256) private _allTokensIndex;
+
+    // Info of each NFT 
+    mapping(uint256 => Achievements) public achievements; 
+
+     // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(address _auth, string memory _uri, string[] memory _strings) ERC721("SPACEWARS", "v1.Achievement") { 
 
         AUTH = _auth;
         auth = IAuth(AUTH);
@@ -64,14 +82,16 @@ contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
         DEAD = auth.DEAD();
         ORACLE = auth.getKojiOracle(); 
         oracle = IOracle(ORACLE);
+        GAMEPASS = auth.getGamePass();
+        gamepass = IGamePass(GAMEPASS); 
 
         URI = _uri;     
 
-        for(uint x = 0; x < strings.length; x++) {
+        for(uint x = 0; x < _strings.length; x++) {
 
             Achievements storage nft = achievements[x];
 
-            nft.name = strings[x];
+            nft.name = _strings[x];
             nft.uri = Strings.toString(x+1);
 
             _NFTIds.increment();
@@ -81,18 +101,18 @@ contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
 
     receive() external payable {}
 
-    function mintAchievement(string memory _holder, string memory data) external nonReentrant {
+    function mintAchievement(string memory _holder, uint data) external nonReentrant {
 
         bool gooddata = false;
 
         require(compareHashes(_msgSender(), _holder), "Sender is not the same as requested recipient");
-        require(nftAuth[_msgSender()], "You must mint a gamepass before minting an achievement");
+        require(gamepass.getNFTAuth(_msgSender()), "You must mint a gamepass before minting an achievement");
 
         for(uint x = 0; x < _NFTIds.current(); x++) {
 
             Achievements storage nft = achievements[x];
 
-            if (keccak256(bytes(abi.encodePacked(nft.name, _holder, x+1))) == keccak256(bytes(abi.encodePacked(data)))) {
+            if (compare(nft.name, _holder, Strings.toString(x+1), data)) {    
 
                 require(!nftMinted[x][_msgSender()], "You already minted this achievement");
 
@@ -109,6 +129,10 @@ contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
                 string memory result = string.concat(metadata, json);
 
                 _setTokenURI(newItemId, string.concat(URI, result));
+
+                nftMinted[x][_msgSender()] = true;
+
+                return;
 
             }
 
@@ -128,27 +152,6 @@ contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
 
     }
 
-    function _transfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override{
-        require(ERC721.ownerOf(tokenId) == from, "ERC721: transfer of token that is not owned");
-        require(to == address(DEAD), "ERC721: transfer must be to the dead address");
-
-        _beforeTokenTransfer(from, to, tokenId);
-
-        // Clear approvals from the previous owner
-        _approve(address(0), tokenId);
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        emit Transfer(from, to, tokenId);
-    }
-
-
     // This will allow to rescue ETH sent by mistake directly to the contract
     function rescueETHFromContract() external onlyOwner {
         address payable _owner = payable(_msgSender());
@@ -165,10 +168,6 @@ contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
         return oracle.getMintUSD(usdvalue);
     }
 
-    function setNFTAuth(address _holder) external {
-        require(msg.sender == IAuth(AUTH).getGamePass(), "Sender is not authorized");
-        nftAuth[_holder] = true;
-    }
 
     function compareHashes(address _address, string memory _holder) internal pure returns (bool) {
 
@@ -193,5 +192,182 @@ contract KojiGame is ERC721Enumerable, ERC165, Ownable, ReentrancyGuard {
         }
         return string(bLower);
     }
+
+    function compare(string memory name, string memory holder, string memory num, uint data) internal pure returns (bool) {
+        return (
+            uint(keccak256(abi.encode(name, holder, num))) == uint(data)
+        );
+
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) public view virtual returns (uint256) {
+        require(index < ERC721.balanceOf(owner), "ERC721Enumerable: owner index out of bounds");
+        return _ownedTokens[owner][index];
+    }
+
+    /**
+     * @dev See {IERC721Enumerable-totalSupply}.
+     */
+    function totalSupply() public view virtual returns (uint256) {
+        return _allTokens.length;
+    }
+
+    /**
+     * @dev See {IERC721Enumerable-tokenByIndex}.
+     */
+    function tokenByIndex(uint256 index) public view virtual returns (uint256) {
+        require(index < totalSupply(), "ERC721Enumerable: global index out of bounds");
+        return _allTokens[index];
+    }
+
+    /**
+     * @dev See {ERC721-_beforeTokenTransfer}.
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal virtual override(ERC721) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+
+        if (batchSize > 1) {
+            // Will only trigger during construction. Batch transferring (minting) is not available afterwards.
+            revert("ERC721Enumerable: consecutive transfers not supported");
+        }
+
+        uint256 tokenId = firstTokenId;
+
+        if (from == address(0)) {
+            //_addTokenToAllTokensEnumeration(tokenId);
+        } else if (from != to) {
+            _removeTokenFromOwnerEnumeration(from, tokenId);
+        }
+        if (to == address(0)) {
+            _removeTokenFromAllTokensEnumeration(tokenId);
+        } else if (to != from) {
+            _addTokenToOwnerEnumeration(to, tokenId);
+        }
+    }
+
+    /**
+     * @dev Private function to add a token to this extension's ownership-tracking data structures.
+     * @param to address representing the new owner of the given token ID
+     * @param tokenId uint256 ID of the token to be added to the tokens list of the given address
+     */
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        uint256 length = ERC721.balanceOf(to);
+        _ownedTokens[to][length] = tokenId;
+        _ownedTokensIndex[tokenId] = length;
+    }
+
+    /**
+     * @dev Private function to add a token to this extension's token tracking data structures.
+     * @param tokenId uint256 ID of the token to be added to the tokens list
+     */
+    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
+
+    /**
+     * @dev Private function to remove a token from this extension's ownership-tracking data structures. Note that
+     * while the token is not assigned a new owner, the `_ownedTokensIndex` mapping is _not_ updated: this allows for
+     * gas optimizations e.g. when performing a transfer operation (avoiding double writes).
+     * This has O(1) time complexity, but alters the order of the _ownedTokens array.
+     * @param from address representing the previous owner of the given token ID
+     * @param tokenId uint256 ID of the token to be removed from the tokens list of the given address
+     */
+    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
+        // To prevent a gap in from's tokens array, we store the last token in the index of the token to delete, and
+        // then delete the last slot (swap and pop).
+
+        uint256 lastTokenIndex = ERC721.balanceOf(from) - 1;
+        uint256 tokenIndex = _ownedTokensIndex[tokenId];
+
+        // When the token to delete is the last token, the swap operation is unnecessary
+        if (tokenIndex != lastTokenIndex) {
+            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
+
+            _ownedTokens[from][tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+            _ownedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+        }
+
+        // This also deletes the contents at the last position of the array
+        delete _ownedTokensIndex[tokenId];
+        delete _ownedTokens[from][lastTokenIndex];
+    }
+
+    /**
+     * @dev Private function to remove a token from this extension's token tracking data structures.
+     * This has O(1) time complexity, but alters the order of the _allTokens array.
+     * @param tokenId uint256 ID of the token to be removed from the tokens list
+     */
+    function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
+        // To prevent a gap in the tokens array, we store the last token in the index of the token to delete, and
+        // then delete the last slot (swap and pop).
+
+        uint256 lastTokenIndex = _allTokens.length - 1;
+        uint256 tokenIndex = _allTokensIndex[tokenId];
+
+        // When the token to delete is the last token, the swap operation is unnecessary. However, since this occurs so
+        // rarely (when the last minted token is burnt) that we still do the swap here to avoid the gas cost of adding
+        // an 'if' statement (like in _removeTokenFromOwnerEnumeration)
+        uint256 lastTokenId = _allTokens[lastTokenIndex];
+
+        _allTokens[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+        _allTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+
+        // This also deletes the contents at the last position of the array
+        delete _allTokensIndex[tokenId];
+        _allTokens.pop();
+    }
+
     
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override(ERC721) returns (string memory) {
+        _requireMinted(tokenId);
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    /**
+     * @dev See {ERC721-_burn}. This override additionally checks to see if a
+     * token-specific URI was set for the token, and if so, it deletes the token URI from
+     * the storage mapping.
+     */
+    function _burn(uint256 tokenId) internal virtual override(ERC721) {
+        super._burn(tokenId);
+
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
+    }
+        
 }
